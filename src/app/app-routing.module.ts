6c6f75e96d06eb0723c40ca29b3704f9ad617c20
Lemasson Steven <lemasson.steven@gmail.com>
2023-01-10 18:13:37 +0100

    creation struct du projet debut todo service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'add-todo', loadChildren: () => import('./add-todo/add-todo.module').then(m => m.AddTodoModule) }, { path: 'add-todo', loadChildren: () => import('./feature/add-todo/add-todo.module').then(m => m.AddTodoModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosListComponent } from './feature/todo/todos-list/todos-list.component';
+import { TodoDetailsComponent } from './feature/todo/todo-details/todo-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosListComponent,
+    TodoDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
